0a523f30236c9b758bd8a69b76f544e303669d39
jason <jason@herman-scheer.com>
2022-01-20 10:05:44 -1000

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Who loves you pretty baby',
+  },
+  {
+    id: 2,
+    rating: 5,
+    text: 'Whos gonna help you through the night',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Who loves you pretty mama',
+  },
+]
+
+export default FeedbackData
